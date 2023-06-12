aa0032907241bb7687ee3b0a6621f87423310f62
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2023-06-12 20:53:43 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponINC
+{
+    internal class Program
+    {
+        class Weapon
+        {
+            private int _damage;
+            private int _bullets;
+
+            public void Fire(Player player)
+            {
+                player.TakeDamage(_damage);
+                _bullets -= 1;
+            }
+        }
+
+        class Player
+        {
+            private int _health;
+
+            public void TakeDamage(int damage)
+            {
+                if (damage > 0)
+                    _health -= damage;
+            }
+        }
+
+        class Bot
+        {
+            private Weapon _weapon;
+
+            public void OnSeePlayer(Player player)
+            {
+                _weapon.Fire(player);
+            }
+        }
+    }
+}
